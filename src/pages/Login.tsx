f2144bf6dcc58e502e12a11f175e69f9c01993e4
itscaiocunha <caio.cunha@sou.unifeob.edu.br>
2025-02-20 10:28:15 -0300

    feat: new url api

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -20,7 +20,7 @@ const Login = () => {
     }
 
     try {
-      const response = await fetch("https://apitedie.vercel.app/api/auth/login", {
+      const response = await fetch("https://tedie-api.vercel.app/api/login", {
         method: "POST",
         headers: {
           "Content-Type": "application/json"
