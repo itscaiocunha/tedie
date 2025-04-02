b84ba3845d3b18fec63efb13095afc8d3327e033
itscaiocunha <caio.cunha@sou.unifeob.edu.br>
2025-04-02 17:06:55 -0300

    feat: remove suppliers

--- src/App.tsx
+++ src/App.tsx
@@ -16,15 +16,6 @@ import Finally from "./pages/Finally";
 import Order from "./pages/Order";
 import ProductDetails from "./pages/Produto";
 import About from "./pages/About";
-
-import LoginFornecedor from "./pages/fornecedor/Login"
-import Dashboard from "./pages/fornecedor/Dashboard";
-import Produtos from "./pages/fornecedor/Produtos";
-import Pedido from "./pages/fornecedor/Pedidos";
-import Deliveries from "./pages/fornecedor/Entrega";
-import Relatorio from "./pages/fornecedor/Relatorio";
-import Configuracao from "./pages/fornecedor/Configuracao";
-
 import NotFound from "./pages/NotFound";
 
 const queryClient = new QueryClient();
@@ -47,15 +38,6 @@ const App = () => (
         <Route path="/creator" element={<Creator />} />
         <Route path="/privacy" element={<Privacy />} />
         <Route path="/terms" element={<Terms />} />
-        
-        <Route path="fornecedor/login" element={<LoginFornecedor />} />
-        <Route path="fornecedor/dashboard" element={<Dashboard />} />
-        <Route path="fornecedor/produtos" element={<Produtos />} />
-        <Route path="fornecedor/pedidos" element={<Pedido />} />
-        <Route path="fornecedor/entrega" element={<Deliveries />} />
-        <Route path="fornecedor/relatorio" element={<Relatorio />} />
-        <Route path="fornecedor/configuracao" element={<Configuracao />} />
-
         <Route path="*" element={<NotFound />} />
       </Routes>
     </TooltipProvider>
